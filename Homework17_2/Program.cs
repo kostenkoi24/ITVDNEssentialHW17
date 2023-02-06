4a749345342835a46a2232bcbfa6226375a957c4
kostenkoi24 <kostenko.i@gmail.com>
2023-02-06 19:35:00 +0200

    Homework17

--- Homework17_2/Program.cs
+++ Homework17_2/Program.cs
@@ -1,12 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Homework17_2
 {
+
+    public class Car
+    {
+       public string marka;
+       public string model;
+       public int year;
+       public string color;
+    }
+
+    public class CarOwner
+    {
+        public string name;
+        public string phone;
+        public Car car;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var cars = new List<Car>()
+            {
+                new Car()
+                {
+                    marka = "Opel",
+                    model = "Insignia",
+                    year = 2020,
+                    color = "grey"
+                },
+                new Car()
+                {
+                    marka = "Nissan",
+                    model = "Leaf",
+                    year = 2014,
+                    color = "white"
+                },
+                new Car()
+                {
+                    marka = "Shkoda",
+                    model = "Rapid",
+                    year = 2012,
+                    color = "black"
+                }
+            };
+
+            var owners = new List<CarOwner>()
+            {
+                new CarOwner()
+                {
+                    name = "Andriy",
+                    phone = "0670202022",
+                    car = cars[0]
+                },
+                new CarOwner()
+                {
+                    name = "Sergiy",
+                    phone = "067030303",
+                    car = cars[1]
+                },
+                new CarOwner()
+                {
+                    name = "Don Pedro",
+                    phone = "067040404",
+                   car = cars[2]
+                }
+            };
+
+
+            var query = from owner in owners
+                        where owner.name == "Don Pedro"
+                        orderby owner.name
+                        select new
+                        {
+                            name = owner.name,
+                            phone = owner.phone,
+                            car = owner.car
+                        };
+
+
+
+            /*Використовуючи найпростіший запит LINQ, виведіть на екран інформацію про покупця одного з автомобілів і повну характеристику придбаної ним моделі. */
+
+            foreach (var owner in query)
+            {
+                Console.WriteLine($"Owner name: {owner.name} \n Owner phone: {owner.phone} \n " +
+                    $"Car info: " +
+                    $"\n\t Marka: {owner.car.marka}" +
+                    $"\n\t Model: {owner.car.model}" +
+                    $"\n\t Year: {owner.car.year}" +
+                    $"\n\t Color: {owner.car.color}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Homework17_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace Homework17_3
+{
+
+    class Translator
+    {
+        public static IEnumerable Words()
+        {
+            yield return new { Key = "сонце", Value = "sun" };
+            yield return new { Key = "вітер", Value = "wind" };
+            yield return new { Key = "хмара", Value = "cloud" };
+            yield return new { Key = "дощ", Value = "rain" };
+            yield return new { Key = "сніг", Value = "snow" };
+            yield return new { Key = "блискавка", Value = "lightning" };
+            yield return new { Key = "спека", Value = "Heat" };
+            yield return new { Key = "холод", Value = "Cold" };
+            yield return new { Key = "температура", Value = "temperature" };
+            yield return new { Key = "туман", Value = "fog" };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            
+            foreach (dynamic item in Translator.Words())
+            {
+                Console.WriteLine($"UA = {item.Key}; EN = {item.Value}");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
